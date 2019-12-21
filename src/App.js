dcad8b34df11810e5840f7a123aa3f5a23075564
isihyeong <buzzcatfish@gmail.com>
2019-12-21 15:43:47 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-function App() {
+const App = () => {
     return (
         <div className="App">
-
+            <div className="number">0</div>
+            <div className="buttons">
+                <button type="button" className="increase">+</button>
+                <button type="button" className="reset">reset</button>
+                <button type="button" className="decrease">-</button>
+            </div>
         </div>
     );
-}
+};
 
 export default App;
